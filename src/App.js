206056146529fb3648fe46363fc6c0f9a46b6ea6
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2018-12-12 19:38:02 -0800

    split checkbox and input into functional components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import CheckBox from './components/checkbox'
+import InputText from './components/inputtext'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1> To-Do </h1>
+        <div className="todo">
+          <CheckBox />
+          <InputText />
+        </div>
       </div>
     );
   }
---
+++ src/components/checkbox.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function CheckBox(props){
+  return(
+    <input type="checkbox"/>
+  )
+}
